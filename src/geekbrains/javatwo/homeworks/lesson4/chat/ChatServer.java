0874e3055b651653f581bbeb109f13151ec105c9
Sergei <njbots@yandex.ru>
2020-04-20 02:08:02 +0300

    lesson5

---
+++ src/geekbrains/javatwo/homeworks/lesson4/chat/ChatServer.java
@@ -0,0 +1,25 @@
+package geekbrains.javatwo.homeworks.lesson4.chat;
+
+
+import geekbrains.javatwo.homeworks.lesson4.network.ServerSocketThread;
+
+public class ChatServer {
+
+    ServerSocketThread server;
+
+    public void start(int port) {
+        if (server != null && server.isAlive())
+            System.out.println("Already running");
+        else
+            server = new ServerSocketThread("Server", port);
+    }
+
+    public void stop() {
+        if (server == null || !server.isAlive()) {
+            System.out.println("Nothing to stop");
+        } else {
+            server.interrupt();
+        }
+    }
+
+}
---
+++ src/geekbrains/javatwo/homeworks/lesson4/network/ServerSocketThread.java
@@ -0,0 +1,27 @@
+package geekbrains.javatwo.homeworks.lesson4.network;
+
+public class ServerSocketThread extends Thread {
+
+    private final int port;
+
+    public ServerSocketThread(String name, int port) {
+        super(name);
+        this.port = port;
+        start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Server started at port: " + port);
+        while (!isInterrupted()) {
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                interrupt();
+            }
+            System.out.println("SST is working");
+        }
+        System.out.println("Server stopped");
+
+    }
+}
